1162bb9623132eb07c18e5180e4f3efe1b4dfe68
kaua vitorio <kauavitorioof@gmail.com>
2020-10-29 17:11:15 -0300

    Criada tela de de splash e tela de login

--- app/src/main/java/com/example/systemstrength/MainActivity.java
+++ app/src/main/java/com/example/systemstrength/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.systemstrength;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-// test
+    private static int TIME_SPLASH = 1500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent irparateladelogin = new Intent(MainActivity.this,LoginActivity.class);
+                finish();
+                startActivity(irparateladelogin);
+            }
+        },TIME_SPLASH);
     }
 }
