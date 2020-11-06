470954f7386f0944ae5f6cab68ce36ea5f8473c8
kaua vitorio <kauavitorioof@gmail.com>
2020-11-06 11:14:05 -0300

    Create new navigationbar on PrincipalActivity and adjust in layout

--- app/src/main/java/com/example/systemstrength/PrincipalActivity.java
+++ app/src/main/java/com/example/systemstrength/PrincipalActivity.java
@@ -1,14 +1,29 @@
 package com.example.systemstrength;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class PrincipalActivity extends AppCompatActivity {
+    LinearLayout linearbtnhomeprincipal, linearbtncontatoprincipal, linearbtnservicosprincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        linearbtnhomeprincipal = findViewById(R.id.linearbtnhomeprincipal);
+        linearbtncontatoprincipal = findViewById(R.id.linearbtncontatoprincipal);
+        linearbtnservicosprincipal = findViewById(R.id.linearbtnservicosprincipal);
+
+        linearbtnhomeprincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(PrincipalActivity.this, "Você já está aqui!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
