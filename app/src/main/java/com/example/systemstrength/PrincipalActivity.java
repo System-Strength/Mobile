85ab15a285537161ac826b75f0ed3f2abc7fd6f7
kaua vitorio <kauavitorioof@gmail.com>
2020-11-16 17:24:16 -0300

    Fix bug in animation

--- app/src/main/java/com/example/systemstrength/PrincipalActivity.java
+++ app/src/main/java/com/example/systemstrength/PrincipalActivity.java
@@ -4,23 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class PrincipalActivity extends AppCompatActivity {
-    LinearLayout linearbtnhomeprincipal, linearbtncontatoprincipal, linearbtnservicosprincipal;
-    TextView txtnomeusu;
-    CardView cardviewbtnmorenossaequipe;
-    ImageView imgavatarusu;
-    String nomeusu="Seu nome";
-    String semnomeusu="Anonimo";
+import com.example.systemstrength.Classes.Login.DaoLogins;
+import com.example.systemstrength.Classes.Login.DtoLogins;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *  Copyright (c) 2020 System Strength
+ *  Official repository https://github.com/System-Strength/Mobile
+ *  Responsible developer: https://github.com/Kauavitorio
+ **/
+
+public class PrincipalActivity extends AppCompatActivity {
+    LinearLayout linearbtnhomeprincipal, linearbtncontatoprincipal, linearbtnservicosprincipal, linearbtnclienteprincipal;
+    TextView txtnomeusu, txthoraatual, txtcargoatual, txtproximareuniao;
+    //ImageView imgavatarusu;
+    ConstraintLayout constraintlayoutperfilusu;
+    CardView  cardviewbtnlermaisjava, cardviewbtnlermaiscsharp, cardviewbtnlermaisjavascript, cardviewbtnlermaishtml, cardviewbtnlermaiscss;
+    String cpfrecebido;
+    String horarecebida;
+
+    //  cardviewbtnlermaisjava
+    //  cardviewbtnlermaiscsharp
+    //  cardviewbtnlermaisjavascript
+    //  cardviewbtnlermaishtml
+    //  cardviewbtnlermaiscss
+
+
+    @SuppressLint("SimpleDateFormat")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,36 +48,53 @@ public class PrincipalActivity extends AppCompatActivity {
         linearbtnhomeprincipal = findViewById(R.id.linearbtnhomeprincipal);
         linearbtncontatoprincipal = findViewById(R.id.linearbtncontatoprincipal);
         linearbtnservicosprincipal = findViewById(R.id.linearbtnservicosprincipal);
+        linearbtnclienteprincipal = findViewById(R.id.linearbtnclienteprincipal);
+        constraintlayoutperfilusu = findViewById(R.id.constraintlayoutperfilusu);
         txtnomeusu = findViewById(R.id.txtnomeusu);
-        imgavatarusu = findViewById(R.id.imgavatarusu);
-        cardviewbtnmorenossaequipe = findViewById(R.id.cardviewbtnmorenossaequipe);
+        txthoraatual = findViewById(R.id.txthoraatual);
+        //imgavatarusu = findViewById(R.id.imgavatarusu);
+        txtcargoatual = findViewById(R.id.txtcargoatual);
+        txtproximareuniao = findViewById(R.id.txtproximareuniao);
 
-        //  Get Name of user
+        //  Taking the current time and placing it on TextView
+        horarecebida = new SimpleDateFormat("HH:mm").format(new Date());
+        txthoraatual.setText(horarecebida);
+
+        //  Get Name of user Turn on this later
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        nomeusu = bundle.getString("nomeusu");
-        if (nomeusu.equals("")){
-            txtnomeusu.setText(semnomeusu);
-            Drawable drawable= getResources().getDrawable(R.drawable.iconeanonimos);
-            imgavatarusu.setImageDrawable(drawable);
-        }
-        else{
-            txtnomeusu.setText(nomeusu);
+        cpfrecebido = bundle.getString("cpffunc");
+        DaoLogins daoLogins = new DaoLogins(PrincipalActivity.this);
+        DtoLogins dtoLogins = daoLogins.verificarusuario(cpfrecebido);
+        txtnomeusu.setText(dtoLogins.getNomefunc());
+        txtcargoatual.setText(dtoLogins.getCargofunc());
+        if (dtoLogins.getUltamareufunc() == null || dtoLogins.getUltamareufunc().equals("")){
+            txtproximareuniao.setText("Não há reunião agendada");
+        }else {
+            txtproximareuniao.setText(dtoLogins.getUltamareufunc());
         }
 
-        linearbtnhomeprincipal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(PrincipalActivity.this, "Você já está aqui!", Toast.LENGTH_SHORT).show();
-            }
+        constraintlayoutperfilusu.setOnClickListener(v -> {
+            Intent irparaperfildeusuario = new Intent(PrincipalActivity.this,PerfilUsuarioActivity.class);
+            irparaperfildeusuario.putExtra("cpfusu",cpfrecebido);
+            startActivity(irparaperfildeusuario);
+            finish();
         });
 
-        cardviewbtnmorenossaequipe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent irparanossaequipe = new Intent(PrincipalActivity.this,NossaEquipeActivity.class);
-                startActivity(irparanossaequipe);
-            }
+
+        linearbtnhomeprincipal.setOnClickListener(v -> Toast.makeText(PrincipalActivity.this, "Você já está aqui!", Toast.LENGTH_SHORT).show());
+
+        linearbtnclienteprincipal.setOnClickListener(v -> {
+            Intent irparaclientes = new Intent(PrincipalActivity.this,ClientesActivity.class);
+            irparaclientes.putExtra("cpfusu",cpfrecebido);
+            startActivity(irparaclientes);
+            finish();
         });
     }
 }
+
+/*
+ *  Copyright (c) 2020 System Strength
+ *  Official repository https://github.com/System-Strength/Mobile
+ *  Responsible developer: https://github.com/Kauavitorio
+ */
