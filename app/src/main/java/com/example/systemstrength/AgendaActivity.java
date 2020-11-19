781eb55d24ba125c358e4ea6b1784298993d8c40
kaua vitorio <kauavitorioof@gmail.com>
2020-11-19 16:38:13 -0300

    New animation

--- app/src/main/java/com/example/systemstrength/AgendaActivity.java
+++ app/src/main/java/com/example/systemstrength/AgendaActivity.java
@@ -1,19 +1,39 @@
 package com.example.systemstrength;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.example.systemstrength.Classes.Agenda.DaoAgenda;
+import com.example.systemstrength.Classes.Agenda.DtoAgenda;
+
+import java.util.ArrayList;
 
 public class AgendaActivity extends AppCompatActivity {
     LinearLayout linearbtnhomeagenda;
-    CardView cardviewloadingpricipalagenda, cardviewnovoagendamento, cardviewanimacaonovoagendamento;
+    CardView cardviewloadingpricipalagenda, cardviewnovoagendamento, cardviewanimacaonovoagendamento, cardviewanimationdeleteagenda;
+    ListView listviewagenda;
+    LottieAnimationView animationdeleteagenda;
+    ArrayList<DtoAgenda> arraylistagenda;
+    DtoAgenda agenda;
+    DaoAgenda daoAgenda = new DaoAgenda(AgendaActivity.this);
+    Dialog myDialog;
     String cpfrecebidobase;
 
     @Override
@@ -24,11 +44,25 @@ public class AgendaActivity extends AppCompatActivity {
         cardviewloadingpricipalagenda = findViewById(R.id.cardviewloadingpricipalagenda);
         cardviewnovoagendamento = findViewById(R.id.cardviewnovoagendamento);
         cardviewanimacaonovoagendamento = findViewById(R.id.cardviewanimacaonovoagendamento);
+        listviewagenda = findViewById(R.id.listviewagenda);
+        cardviewanimationdeleteagenda = findViewById(R.id.cardviewanimationdeleteagenda);
+        animationdeleteagenda = findViewById(R.id.animationdeleteagenda);
+        myDialog = new Dialog(this);
 
         //  Get some information
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         cpfrecebidobase = bundle.getString("cpfusu");
+
+        DaoAgenda daoAgenda= new DaoAgenda(AgendaActivity.this);
+        arraylistagenda = daoAgenda.consultarTodaagenda();
+        atualizarlistview();
+
+        listviewagenda.setOnItemLongClickListener((parent, view, position, id) -> {
+            agenda = arraylistagenda.get(position);
+            registerForContextMenu(listviewagenda);
+            return false;
+        });
 
         cardviewnovoagendamento.setOnClickListener(v -> {
             cardviewnovoagendamento.setVisibility(View.GONE);
@@ -51,5 +85,82 @@ public class AgendaActivity extends AppCompatActivity {
                 finish();
             },1500);
         });
+    }
+
+    //  Create method for update  ListView
+    private void atualizarlistview() {
+        ArrayAdapter adapter = new ArrayAdapter(AgendaActivity.this, android.R.layout.simple_list_item_1, arraylistagenda);
+        listviewagenda.setAdapter(adapter);
+    }
+
+    //  Create method for delete some client of  ListView
+    private void excluir() {
+        AlertDialog.Builder msg = new AlertDialog.Builder(AgendaActivity.this);
+        msg.setMessage("Confirma a exclusão?");
+        msg.setPositiveButton("Sim", (dialogInterface, i) -> {
+            int deletados = daoAgenda.excluir(agenda);
+            if (deletados>0){
+                cardviewanimationdeleteagenda.setVisibility(View.VISIBLE);
+                animationdeleteagenda.playAnimation();
+                new Handler().postDelayed(() -> {
+                    Toast.makeText(AgendaActivity.this, "Excluído com sucesso!", Toast.LENGTH_SHORT).show();
+                    arraylistagenda = daoAgenda.consultarTodaagenda();
+                    atualizarlistview();
+                    cardviewanimationdeleteagenda.setVisibility(View.GONE);
+                    animationdeleteagenda.pauseAnimation();
+                },2000);
+            } else {
+                Toast.makeText(AgendaActivity.this, "Erro ao Excluir!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        msg.setNegativeButton("Não", null);
+        msg.show();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.setHeaderTitle("• " + agenda.getNomecliente()+ " •");
+        menu.add(0,0,0,"Detalhes");
+        menu.add(0,1,1,"Editar");
+        menu.add(0,2,2,"Excluir");
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == 0){
+            aparecerpopup();
+        }else if (item.getItemId()  == 1){
+
+        }else if (item.getItemId() == 2){
+            excluir();
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+    public void aparecerpopup() {
+        TextView txtnomeclientedetalhes, txtcnpjclientedetalhes, txtdatadetalhes, txthourdetalhes, txtlocaldetalhes, txtdescdetalhes;
+        LinearLayout btnclosedetalhes;
+        myDialog.setContentView(R.layout.popupdetalhesagenda);
+        txtnomeclientedetalhes = (TextView) myDialog.findViewById(R.id.txtnomeclientedetalhes);
+        txtcnpjclientedetalhes = (TextView) myDialog.findViewById(R.id.txtcnpjclientedetalhes);
+        txtdatadetalhes = (TextView) myDialog.findViewById(R.id.txtdatadetalhes);
+        txthourdetalhes = (TextView) myDialog.findViewById(R.id.txthourdetalhes);
+        txtlocaldetalhes = (TextView) myDialog.findViewById(R.id.txtlocaldetalhes);
+        txtdescdetalhes = (TextView) myDialog.findViewById(R.id.txtdescdetalhes);
+        btnclosedetalhes = (LinearLayout) myDialog.findViewById(R.id.btnclosedetalhes);
+
+        txtnomeclientedetalhes.setText(agenda.getNomecliente());
+        txtcnpjclientedetalhes.setText(agenda.getCnpjcliente());
+        txtdatadetalhes.setText(agenda.getDataagendamento());
+        txthourdetalhes.setText(agenda.getHoraagendamento());
+        txtlocaldetalhes.setText(agenda.getLocaldoagendamento());
+        txtdescdetalhes.setText(agenda.getDescricaoagendamento());
+
+        btnclosedetalhes.setOnClickListener(v1 -> myDialog.dismiss());
+
+        myDialog.show();
     }
 }
--- app/src/main/java/com/example/systemstrength/NovoAgendamentoActivity.java
+++ app/src/main/java/com/example/systemstrength/NovoAgendamentoActivity.java
@@ -1,23 +1,32 @@
 package com.example.systemstrength;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.example.systemstrength.Classes.Agenda.DaoAgenda;
+import com.example.systemstrength.Classes.Agenda.DtoAgenda;
 import com.example.systemstrength.Classes.Clientes.DaoClientes;
 import com.example.systemstrength.Classes.Clientes.DtoClientes;
 
 public class NovoAgendamentoActivity extends AppCompatActivity {
     LinearLayout linearvoltaraagenda, linearselecionaroutrocliente;
-    LottieAnimationView animacaovoltaraagenda;
-    CardView cardviewbtnescolhercliente;
+    LottieAnimationView animacaovoltaraagenda, loadingcadastro, animacaocorreto;
+    TextView txtsalvaragenda;
+    CardView cardviewbtnescolhercliente, cardviewbtnsalvarnovoagendamento;
     EditText edittextclientenovoagendamento, edittextcnpjclientenovoagendamento, edittextdatanovoagendamento, edittexthoranovoagendamento, edittextlocalnovoagendamento, edittextdescnovoagendamento;
     String cpfrecebidodaagenda;
     String clienteescolhido;
@@ -30,12 +39,19 @@ public class NovoAgendamentoActivity extends AppCompatActivity {
         linearselecionaroutrocliente = findViewById(R.id.linearselecionaroutrocliente);
         animacaovoltaraagenda = findViewById(R.id.animacaovoltaraagenda);
         cardviewbtnescolhercliente = findViewById(R.id.cardviewbtnescolhercliente);
+        cardviewbtnsalvarnovoagendamento = findViewById(R.id.cardviewbtnsalvarnovoagendamento);
         edittextclientenovoagendamento = findViewById(R.id.edittextclientenovoagendamento);
         edittextcnpjclientenovoagendamento = findViewById(R.id.edittextcnpjclientenovoagendamento);
         edittextdatanovoagendamento = findViewById(R.id.edittextdatanovoagendamento);
         edittexthoranovoagendamento = findViewById(R.id.edittexthoranovoagendamento);
         edittextlocalnovoagendamento = findViewById(R.id.edittextlocalnovoagendamento);
         edittextdescnovoagendamento = findViewById(R.id.edittextdescnovoagendamento);
+        loadingcadastro = findViewById(R.id.loadingcadastro);
+        animacaocorreto = findViewById(R.id.animacaocorreto);
+        txtsalvaragenda = findViewById(R.id.txtsalvaragenda);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        linearselecionaroutrocliente.setVisibility(View.GONE);
 
         edittextcnpjclientenovoagendamento.addTextChangedListener(MaskEditUtil.mask(edittextcnpjclientenovoagendamento, MaskEditUtil.FORMAT_CNPJ));
         edittextdatanovoagendamento.addTextChangedListener(MaskEditUtil.mask(edittextdatanovoagendamento, MaskEditUtil.FORMAT_DATE));
@@ -59,6 +75,73 @@ public class NovoAgendamentoActivity extends AppCompatActivity {
             finish();
         });
 
+        linearselecionaroutrocliente.setOnClickListener(v -> {
+            Intent irparalistadeclientes = new Intent(NovoAgendamentoActivity.this,ListadeClientesActivity.class);
+            irparalistadeclientes.putExtra("cpfusu",cpfrecebidodaagenda);
+            startActivity(irparalistadeclientes);
+            finish();
+        });
+
+        cardviewbtnsalvarnovoagendamento.setOnClickListener(v -> {
+            if (edittextclientenovoagendamento.getText().length() == 0){
+                msgselecioneocliente();
+            }else if(edittextcnpjclientenovoagendamento.getText().length() == 0){
+                msgselecioneocliente();
+            }else if(edittextdatanovoagendamento.getText().length() == 0 || edittextdatanovoagendamento.getText().length() < 10){
+                Toast.makeText(NovoAgendamentoActivity.this, "Campo DATA preenchido incorretamente!!", Toast.LENGTH_SHORT).show();
+                edittextdatanovoagendamento.requestFocus();
+                imm.showSoftInput(edittextdatanovoagendamento, InputMethodManager.SHOW_IMPLICIT);
+            }else if(edittexthoranovoagendamento.getText().length() == 0 || edittexthoranovoagendamento.getText().length() < 5){
+                Toast.makeText(NovoAgendamentoActivity.this, "Campo HORA preenchido incorretamente!!", Toast.LENGTH_SHORT).show();
+                edittexthoranovoagendamento.requestFocus();
+                imm.showSoftInput(edittexthoranovoagendamento, InputMethodManager.SHOW_IMPLICIT);
+            }else if(edittextdescnovoagendamento.getText().length() == 0 || edittextdescnovoagendamento.getText().length() < 10){
+                Toast.makeText(NovoAgendamentoActivity.this, "Campo DESCRIÇÃO preenchido incorretamente!!", Toast.LENGTH_SHORT).show();
+                edittextdescnovoagendamento.requestFocus();
+                imm.showSoftInput(edittextdescnovoagendamento, InputMethodManager.SHOW_IMPLICIT);
+            }else {
+                try {
+                    txtsalvaragenda.setVisibility(View.GONE);
+                    loadingcadastro.setVisibility(View.VISIBLE);
+                    loadingcadastro.playAnimation();
+                    DtoAgenda dtoAgenda = new DtoAgenda();
+                    DaoAgenda daoAgenda = new DaoAgenda(NovoAgendamentoActivity.this);
+                    dtoAgenda.setNomecliente(edittextclientenovoagendamento.getText().toString());
+                    dtoAgenda.setCnpjcliente(edittextcnpjclientenovoagendamento.getText().toString());
+                    dtoAgenda.setDataagendamento(edittextdatanovoagendamento.getText().toString());
+                    dtoAgenda.setHoraagendamento(edittexthoranovoagendamento.getText().toString());
+                    dtoAgenda.setLocaldoagendamento(edittextlocalnovoagendamento.getText().toString());
+                    dtoAgenda.setDescricaoagendamento(edittextdescnovoagendamento.getText().toString());
+                    new Handler().postDelayed(() -> {
+                        long linasinseridas = daoAgenda.cadastrar(dtoAgenda);
+                        if (linasinseridas > 0){
+                            loadingcadastro.setVisibility(View.GONE);
+                            loadingcadastro.pauseAnimation();
+                            animacaocorreto.setVisibility(View.VISIBLE);
+                            animacaocorreto.playAnimation();
+                            new Handler().postDelayed(() -> {
+                                Toast.makeText(this, "Cadastrado com sucessso!!", Toast.LENGTH_SHORT).show();
+                                Intent voltaraosclientes = new Intent(NovoAgendamentoActivity.this,AgendaActivity.class);
+                                voltaraosclientes.putExtra("cpfusu",cpfrecebidodaagenda);
+                                startActivity(voltaraosclientes);
+                                finish();
+                            },1000);
+                        }else{
+                            Toast.makeText(this, "Falha ao cadastrar!!\nContate um administrador", Toast.LENGTH_SHORT).show();
+                            new Handler().postDelayed(() -> {
+                                Intent voltaraosclientes = new Intent(NovoAgendamentoActivity.this,AgendaActivity.class);
+                                voltaraosclientes.putExtra("cpfusu",cpfrecebidodaagenda);
+                                startActivity(voltaraosclientes);
+                                finish();
+                            },1000);
+                        }
+                    },1500);
+                }catch (Exception ex){
+                    Toast.makeText(this, "Erro ao cadastrar: "+ ex, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         linearvoltaraagenda.setOnClickListener(v -> {
             animacaovoltaraagenda.playAnimation();
             new Handler().postDelayed(() -> {
@@ -79,7 +162,23 @@ public class NovoAgendamentoActivity extends AppCompatActivity {
             DtoClientes dtoClientes = daoClientes.consultarcliente(clienteescolhido);
             edittextclientenovoagendamento.setText(dtoClientes.getNomecliente());
             edittextcnpjclientenovoagendamento.setText(dtoClientes.getCnpjcliente());
+            linearselecionaroutrocliente.setVisibility(View.VISIBLE);
         }
+    }
+
+    private void msgselecioneocliente(){
+        AlertDialog.Builder msgselectclient = new AlertDialog.Builder(NovoAgendamentoActivity.this);
+        msgselectclient.setIcon(R.drawable.logo);
+        msgselectclient.setTitle("Cliente não selecionado!!");
+        msgselectclient.setMessage("Necessario selecionar o cliente para gerar um novo agendamento");
+        msgselectclient.setPositiveButton("Selecionar Cliente", (dialog, which) -> {
+            Intent irparalistadeclientes = new Intent(NovoAgendamentoActivity.this,ListadeClientesActivity.class);
+            irparalistadeclientes.putExtra("cpfusu",cpfrecebidodaagenda);
+            startActivity(irparalistadeclientes);
+            finish();
+        });
+        msgselectclient.setNeutralButton("Cancelar",null);
+        msgselectclient.show();
     }
 
     @Override
